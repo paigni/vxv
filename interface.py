4fc3610413b8fcdc1d4ec2d4aabf2e1d8195113b
paigni <ikontai4@gmail.com>
2022-09-28 20:24:39 +0600

    Рефакторинг кода

--- interface.py
+++ interface.py
@@ -1,48 +1,27 @@
 import sys
 import time
 
-from service import available_heroes, first_team, second_team, list_of_hero
-from check import check_hero_choice, check_choice
+from service import create_list,team_building,first_team,second_team
+from check import  check_choice
+from collections import Counter
 
 
-def team_building(team: int):
-    if team == 1:
-        print("Выберите героев первой команды")
-    elif team == 2:
-        print("Выберите героев второй команды")
-    user_input = 'd'
-    while not user_input.isdigit():
-        user_input = input()
-        if check_hero_choice(user_input):
-            hc = int(user_input)
-            hero = list_of_hero[hc - 1]
-            print(hero)
-            user_input = 'd'
-            while not user_input.isdigit:
-                user_input = input("Выберите количество")
-                count = 0
-                while count <= int(user_input):
-                    count += 1
-                    if team == 1:
-                        first_team.append(hero)
-                    elif team == 2:
-                        second_team.append(hero)
+def available_heroes():
+    """
+        Выводит список героев доступных на текущий момент
+    """
+    print("Вам доступны следующие герои:\n")
+    count = 0
+    for hero in create_list():
+        count += 1
+        print(f"{count}){hero.name}: здоровье {hero.max_hp} \n"
+              f"урон {hero.damage}, и шанс попадания {hero.damage_chance}")
 
 
-
-def poctor(c):
-        team_building(c)
-        user_input = 'd'
-        while not user_input.isdigit():
-            hc_input = input("Желаете добавить ещё игроков? 1-да, 2 - нет ")
-            if hc_input.isdigit() and check_choice(hc_input):
-                hc_input = int(hc_input)
-                if hc_input == 1:
-                    team_building(c)
-                else:
-                    break
-            else:
-                print("Неверный ввод,повторите попытку")
+def team_view(team:list):
+    team = Counter(team)
+    for key,value in team.items():
+        print(f"{key.name}-{value}")
 
 
 def start_game():
@@ -53,17 +32,35 @@ def start_game():
         Ввод пользователя
     """
 
-    print(f'Добро пожаловать в игру, чтобы начать нажмите 1\n'
-          f'для того чтобы выйти нажмите 2')
-    user_input = 'd'
-    while not user_input.isdigit():
-        user_input = input()
-        if user_input.isdigit() and check_choice(user_input):
-            if int(user_input) == 1:
+    user_input = 4
+    while not check_choice(user_input):
+        user_input = input(f'Добро пожаловать в игру, чтобы начать нажмите 1\n'
+                           f'для того чтобы выйти нажмите 2\n')
+        if int(user_input) == 1:
+            user_input = input("Выберите сторону для выбора персонажей \n 1-Силы света \n 2-Силы тьмы\n")
+            if user_input == '1':
+                print("Выбор персонажей сил света\n")
                 available_heroes()
-                poctor(1)
-                poctor(2)
-                print(first_team,second_team)
+                while user_input !=2:
+                    team_building(1)
+                    user_input = input("Желаете добавить ещё игроков? 1-да, 2 - нет ")
+                print("Выбор персонажей сил тьмы\n")
+                available_heroes()
+                while user_input != 2:
+                    team_building(2)
+                    user_input = input("Желаете добавить ещё игроков? 1-да, 2 - нет ")
+            if user_input == '2':
+                print("Выбор персонажей сил тьмы\n")
+                available_heroes()
+                team_building(2)
+                print("Выбор персонажей сил света\n")
+                available_heroes()
+                team_building(1)
+            print(f"Персонажи сил света:\n{team_view(first_team)}")
+            print(f"Персонажи сил тьмы:\n{team_view(second_team)}")
+            user_input = input("Вы закончили выбор? Начинаем игру? \n 1-нет \n 2-да")
+            if user_input == '2':
+                break
         else:
             print('Неверный ввод,пожалуйста введите значение 1 или 2')
 
