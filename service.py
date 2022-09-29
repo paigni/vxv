cb04888e67b65b5365a76f1e1bf471bcf63dde8f
paigni <ikontai4@gmail.com>
2022-09-29 20:17:38 +0600

    Рефакторинг функции team_building

--- service.py
+++ service.py
@@ -1,10 +1,10 @@
 from hero_class import Penguin, Cat, Grandma, Teenager, \
     Soldier, StarWarsStormtrooper, TrxDinosaur
 
-from check import check_hero_choice,check_choice
+from check import check_hero_choice
 
 
-def create_list():
+def spawn_characters():
     PENGUIN = Penguin()
     CAT = Cat()
     GRANDMA = Grandma()
@@ -21,18 +21,16 @@ second_team = []
 
 
 def team_building(team: int):
-    if not check_choice('5'):
-        print("Выберите героя")
-        user_hero = int(input())
-        if not check_hero_choice(user_hero):
-            hero = create_list()[user_hero - 1]
-            user_input = input("Выберите количество")
-            count = 0
-            while count <= int(user_input):
-                count += 1
-                if team == 1:
-                    first_team.append(hero)
-                elif team == 2:
-                    second_team.append(hero)
-        return first_team,second_team
+    user_hero = int(input("Выберите героя"))
+    if not check_hero_choice(user_hero):
+        hero = spawn_characters()[user_hero - 1]
+        user_input = input("Выберите количество")
+        count = 0
+        while count < int(user_input):
+            count += 1
+            if team == 1:
+                first_team.append(hero)
+            elif team == 2:
+                second_team.append(hero)
+    return first_team, second_team
 
