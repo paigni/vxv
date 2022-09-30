721972cda6f757328ca3552e3cab711ffaa06168
paigni <ikontai4@gmail.com>
2022-09-30 20:14:57 +0600

    Рефакторинг функций start_game,defeated_character,main

--- interface.py
+++ interface.py
@@ -33,19 +33,26 @@ def start_game():
         Ввод пользователя
     """
 
-    user_input = input(f'Добро пожаловать в игру, чтобы начать нажмите 1\n'
-                           f'для того чтобы выйти нажмите 2\n')
-    if int(user_input) == 1:
-        use_input = int(input("Выберите сторону для выбора персонажей \n 1-Силы света \n 2-Силы тьмы\n"))
-        if use_input == 1:
-            print("Выбор персонажей первой команды\n")
-            while user_input != 2:
+    use_input = input(f'Добро пожаловать в игру, чтобы начать нажмите 1\n'
+                      f'для того чтобы выйти нажмите 2\n')
+    if use_input == '1' and use_input.isdigit():
+        user_input = 4
+        while user_input == 1 or 2:
+            user_input = int(input("Выберите сторону для выбора персонажей \n "
+                                   "1-Первая команда \n 2-Вторая команда\n 3-Начать игру\n"))
+            if user_input == 3:
+                if first_team == [] or second_team == []:
+                    print("Для начала игры нужно добавить героев в обе команды")
+                    user_input = 1
+                    continue
+                else:
+                    print("Игра началась")
+                    break
+            elif user_input == 1 or user_input == 2:
+                print("Выбор персонажей\n")
                 show_heroes()
-                team_building(team=1)
-                user_input = int(input("Желаете добавить ещё игроков? 1-да, 2 - нет "))
-                if user_input == 2:
-                    print("Выбор персонажей второй команды\n")
-                    team_building(team=2)
+                while user_input == 1 or 2:
+                    team_building(team=user_input)
                     user_input = int(input("Желаете добавить ещё игроков? 1-да, 2 - нет "))
                     if user_input == 2:
                         print(f"Персонажи первой команды:")
@@ -53,64 +60,93 @@ def start_game():
                         print(f"Персонажи второй команды:")
                         team_view(second_team)
                         break
-        elif use_input == 2:
-            print("Выбор персонажей второй команды\n")
-            show_heroes()
-            while user_input != 2:
-                team_building(team=2)
-                user_input = int(input("Желаете добавить ещё игроков? 1-да, 2 - нет "))
-                if user_input == 2:
-                    print("Выбор персонажей первой команды\n")
-                    show_heroes()
-                    user_input = 1
-                    while user_input != 2:
-                        team_building(team=1)
-                        user_input = int(input("Желаете добавить ещё игроков? 1-да, 2 - нет "))
-                        if user_input == 2:
-                            print(f"Персонажи первой команды:")
-                            team_view(first_team)
-                            print(f"Персонажи второй команды:")
-                            team_view(second_team)
-                            break
-        else:
-            print('Неверный ввод,пожалуйста введите значение 1 или 2')
+                    else:
+                        continue
+    else:
+        print("Неверный ввод,введите 1 или 2")
+        while use_input == '1' and use_input.isdigit():
+            use_input = input()
 
 
 
-def defeated_character(hero_list: list):
+def defeated_character(hero):
     """
     Когда умер один из игроков команды, печатается сообщение
 
 
     Args:
-        hero_list: список героев одной из команд
+        hero: список героев одной из команд
 
     Returns:
     """
-    for hero in hero_list:
-        if not check_hero_hp(hero):
-            hero_list.remove(hero)
-            print(f"Герой {hero.name} мёртв")
-    return hero_list
+    if not hero.current_hp >= 0:
+        print(f"Герой {hero.name} мёртв")
+        return True
+    return False
 
 
 def main():
     start_game()
-    first_team_hero = 0
-    second_team_hero = 0
-    while not first_team or second_team:
-        print("Первая команда нанесла урон:")
-        first_team[first_team_hero].hero_damage(second_team[0])
-        defeated_character(second_team)
-        print("Вторая команда нанесла урон:")
-        second_team[second_team_hero].hero_damage(first_team[0])
-        defeated_character(first_team)
+    copy_first_team = first_team.copy()
+    copy_second_team = second_team.copy()
+    while first_team or second_team:
         if not first_team:
-            print("Победила вторая команда")
-            break
-        elif not second_team:
-            print("Победила первая команда")
-            break
+            sys.exit("Победила вторая команда")
+
+        if not second_team:
+            sys.exit("Победила первая команда")
+
+        if not copy_first_team and first_team != []:
+            copy_first_team = first_team.copy()
+            continue
+
+        if not copy_second_team and second_team != []:
+            copy_second_team = second_team.copy()
+            continue
+
+
+
+        copy_first_team[0].hero_damage(second_team[0])
+        if not defeated_character(second_team[0]):
+            print("Вторая команда нанесла урон:")
+            copy_second_team[0].hero_damage(first_team[0])
+            copy_second_team.pop(0)
+
+            if not defeated_character(first_team[0]):
+                copy_first_team.pop(0)
+                time.sleep(0.1)
+                continue
+
+            if defeated_character(first_team[0]) and first_team != []:
+                first_team.pop(0)
+                copy_first_team = first_team.copy()
+                time.sleep(0.1)
+                continue
+
+
+        elif defeated_character(second_team[0]) and second_team != []:
+            second_team.pop(0)
+
+            if second_team == []:
+                sys.exit("Победила первая команда")
+
+            if first_team == []:
+                sys.exit("Победила вторая команда")
+
+            copy_second_team = second_team.copy()
+            print("Вторая команда нанесла урон:")
+            copy_second_team[0].hero_damage(first_team[0])
+
+            if not defeated_character(first_team[0]):
+                copy_first_team.pop(0)
+                time.sleep(0.1)
+                continue
+
+            elif defeated_character(first_team[0]) and first_team != []:
+                first_team.pop(0)
+                copy_first_team = first_team.copy()
+                time.sleep(0.1)
+                continue
 
 
 
