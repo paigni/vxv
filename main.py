a00dc1370e642508a8338fa3ae1169474a1197b8
paigni <ikontai4@gmail.com>
2022-10-03 20:35:01 +0600

    Рефакторинг кода

--- main.py
+++ main.py
@@ -11,34 +11,34 @@ def main():
     second_team_hero = -1
 
     while True:
-        if not first_team[first_team_hero].current_hp <= 0:
+        first_hero = first_team[first_team_hero]
+        second_hero = second_team[second_team_hero]
+        if not first_hero.current_hp <= 0:
             first_team_hero = update_count(first_team, first_team_hero)
-
         else:
-            print(f'Умер {first_team[first_team_hero].name},игрок первой команды')
-            first_team.remove(first_team[first_team_hero])
+            print(f'Умер {first_hero.name},игрок первой команды')
+            first_team.remove(first_hero)
             if not first_team:
                 sys.exit("Победила вторая команда")
             else:
                 first_team_hero = update_count(first_team, first_team_hero)
 
-        if not second_team[second_team_hero].current_hp <= 0:
+        if not second_hero.current_hp <= 0:
             second_team_hero = update_count(second_team, second_team_hero)
-
         else:
-            print(f'Умер {first_team[first_team_hero].name},игрок второй команды')
-            second_team.remove(second_team[second_team_hero])
+            print(f'Умер {second_hero.name},игрок второй команды')
+            second_team.remove(second_hero)
             if not second_team:
                 sys.exit("Победила первая команда")
             else:
                 second_team_hero = update_count(second_team, second_team_hero)
 
         print("Первая команда нанесла урон:")
-        first_team[first_team_hero].hero_damage(second_team[second_team_hero])
+        first_hero.hero_damage(second_hero)
         time.sleep(0.1)
 
         print("Вторая команда нанесла урон:")
-        second_team[second_team_hero].hero_damage(first_team[first_team_hero])
+        second_hero.hero_damage(first_hero)
         time.sleep(0.1)
 
 
