a1083d8dfb8c1ac960bf9432a195b8266bb462d5
paigni <ikontai4@gmail.com>
2022-10-03 20:24:22 +0600

    обновлён gitignore

--- service.py
+++ service.py
@@ -4,6 +4,7 @@ from hero_class import Penguin, Cat, Grandma, Teenager, \
 from check import check_hero_choice
 
 
+
 def spawn_characters():
     PENGUIN = Penguin()
     CAT = Cat()
@@ -21,16 +22,32 @@ second_team = []
 
 
 def team_building(team: int):
-    user_hero = int(input("Выберите героя"))
-    if not check_hero_choice(user_hero):
-        hero = spawn_characters()[user_hero - 1]
-        user_input = input("Выберите количество")
+    while True:
+        user_hero = input("Выберите героя")
+        if user_hero.isdigit() and not check_hero_choice(int(user_hero)) :
+            hero = spawn_characters()[int(user_hero) - 1]
+            user_input = input("Выберите количество")
+            if user_input.isdigit():
+                count = 0
+                while count < int(user_input):
+                    count += 1
+                    if team == 1:
+                        first_team.append(hero)
+                    elif team == 2:
+                        second_team.append(hero)
+                return first_team, second_team
+            else:
+                print("Количество можно ввести только числом")
+        else:
+            print("Сейчас можно выбрать героя только от 1 до 7")
+
+
+def update_count(team,count):
+    if len(team) <= count + 1:
         count = 0
-        while count < int(user_input):
-            count += 1
-            if team == 1:
-                first_team.append(hero)
-            elif team == 2:
-                second_team.append(hero)
-    return first_team, second_team
+    else:
+        count += 1
+
+    return count
+
 
